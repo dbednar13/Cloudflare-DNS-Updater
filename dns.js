a130c3861b7a9e6e994fb6d4350cae946ee38890
Dan Bednar <dbednar13@gmail.com>
2024-02-16 09:28:43 -0600

    Add logging

--- dns.js
+++ dns.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Logger from './logger.js';
 
 const AUTH_EMAIL = ''; // Your Email here
 const AUTH_TOKEN = ''; // Your api key here
@@ -7,18 +8,21 @@ const ZONE_IDS = ['']; // List of all zones you wish to update
 
 async function getCurrentIPs() {
   const v4 = (await axios.get('http://api.ipify.org')).data;
+  Logger.info(`Received IPv4 ${v4}`);
 
   let v6;
   try {
     v6 = (await axios.get('http://api6.ipify.org')).data;
+    Logger.info(`Received IPv6 ${v6}`);
   } catch {
-    console.log('No IPv6 IP address.');
+    Logger.warn('No IPv6 IP address.');
   }
 
   return { v4, v6 };
 }
 
 async function getZoneData(zoneId) {
+  Logger.info(`Getting zone data for zone ${zoneId}`);
   const zoneData = (
     await axios.get(
       `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`,
@@ -37,24 +41,25 @@ async function getZoneData(zoneId) {
   };
 
   if (zoneData.success) {
+    Logger.info('Received a success message from Cloudflare');
     results.A = zoneData.result.filter((x) => x.type === 'A');
     results.AAAA = zoneData.result.filter((x) => x.type === 'AAAA');
+  } else {
+    Logger.info('Received a non-success message from Cloudflare');
   }
 
   return results;
 }
 
 async function updateEntry(zoneId, data) {
-  await axios.put(
-    `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${data.id}`,
-    data,
-    {
-      headers: {
-        'X-Auth-Key': AUTH_TOKEN,
-        'X-Auth-Email': AUTH_EMAIL,
-      },
-    }
-  );
+  const url = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${data.id}`;
+  Logger.warn(`Updating ${url} with data ${JSON.stringify(data)}`);
+  await axios.put(url, data, {
+    headers: {
+      'X-Auth-Key': AUTH_TOKEN,
+      'X-Auth-Email': AUTH_EMAIL,
+    },
+  });
 }
 
 (async () => {
@@ -65,15 +70,23 @@ async function updateEntry(zoneId, data) {
 
     zoneResults.A.forEach(async (a) => {
       if (a.content !== ips.v4) {
+        Logger.info(`Updating ${a.name} from ip ${a.content} to ${ips.v4}`);
         a.content = ips.v4;
         await updateEntry(zoneId, a);
+      } else {
+        Logger.info(`IPv4 matches for ${a.name}`);
       }
     });
 
     zoneResults.AAAA.forEach(async (aaaa) => {
       if (aaaa.content !== ips.v6) {
+        Logger.info(
+          `Updating ${aaaa.name} from ip ${aaaa.content} to ${ips.v6}`
+        );
         aaaa.content = ips.v6;
         await updateEntry(zoneId, aaaa);
+      } else {
+        Logger.info(`IPv6 matches for ${aaaa.name}`);
       }
     });
   });
